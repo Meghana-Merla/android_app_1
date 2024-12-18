4dfb32021225f906550f2bc5df05da21b8b733fd
Meghana-Merla <24je0654@iitism.ac.in>
2024-12-18 17:57:16 +0530

    Main Activity Completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ implementation("androidx.core:core-ktx:1.12.0")
 implementation("androidx.appcompat:appcompat:1.6.1")
 implementation("com.google.android.material:material:1.11.0")
 implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.androidx.activity)
 
 // Testing dependencies (optional)
 testImplementation("junit:junit:4.13.2")
